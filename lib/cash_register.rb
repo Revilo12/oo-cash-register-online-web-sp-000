f784e71d0e65539109945e6556b2aff57b9befaf
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-10 13:27:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,18 @@
 class CashRegister
   attr_accessor :total, :discount, :last_transaction_amount
 
+  ITEMS = []
+
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+  end
+
+  def add_item(name, price, quantity = 1)
+    quantity.times do
+      ITEMS << name
+      @total += price
+    end
   end
 
 end
