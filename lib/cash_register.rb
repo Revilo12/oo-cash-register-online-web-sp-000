1e357517a3a5214b8c84af82c4af8b14e6cb13ea
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-10 13:20:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister
-  attr_accessor :discount, :last_transaction_amount
+  attr_accessor :total, :discount, :last_transaction_amount
 
   def initialize(discount = 0)
+    @total = 0
     @discount = discount
   end
 
